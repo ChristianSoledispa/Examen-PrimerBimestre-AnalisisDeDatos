31bf3713fe5a89a6970d892a253389516bf55784
ChristianSoledispa <85883884+ChristianSoledispa@users.noreply.github.com>
2021-07-30 19:19:03 -0500

    Generación de datos desde twitter
    juegos olimpicos, tokyo2020 hashtags

---
+++ 1.py
@@ -0,0 +1,48 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+
+###API ########################
+ckey = "IP59RUVsARhC6e2DFCzLVOySI"
+csecret = "AzXNZwJlLzffQq594VeiKPtm1NTd7zlgDGIMQaj9CRZdazKG4y"
+atoken = "1415810816264347658-UBlBSxAL25iujJqJaiULgSKC94xHzQ"
+asecret = "TgfinkDMF98pJVohQBJW7mOxMPpM0bBwIgJXMO1Lm9Lbx"
+#####################################
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = db.save(dictTweet)
+            print ("SAVED" + str(doc) +"=>" + str(data))
+        except:
+            print ("Already exists")
+            pass
+        return True
+    
+    def on_error(self, status):
+        print (status)
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://ChristianSoledispa:soledispa1@localhost:5984/')  #('http://115.146.93.184:5984/')
+try:
+    db = server.create('tokyo2020')
+except:
+    db = server['tokyo2020']
+    
+'''===============LOCATIONS=============='''    
+
+#twitterStream.filter(locations=[11.4037,41.7901,13.9414,43.568])  
+twitterStream.filter(track=['tokyo2020','juegos_olimpicos'])
